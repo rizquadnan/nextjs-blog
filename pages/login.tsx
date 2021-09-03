cf8539c82b7d63a65ff441c5fec95f4c2d245a83
Muhammad Adnan R <muhammad.adnan@ruangguru.com>
2021-09-03 15:32:15 +0700

    finishing login

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,4 +1,5 @@
-import { Flex, Heading, Input, Button, useColorMode, useColorModeValue } from "@chakra-ui/react";
+import { Flex, Box, Heading, Input, Button, useColorMode, useColorModeValue, Text } from "@chakra-ui/react";
+import Link from 'next/link'
 
 const Login = () => {
     const { toggleColorMode } = useColorMode();
@@ -6,13 +7,18 @@ const Login = () => {
 
     return (
         <Flex height="100vh" alignItems="center" justifyContent="center">
-            <Flex direction="column" background={formBackground} padding={12} rounded={6}>
-                <Heading mb={6}>Log in</Heading>
-                <Input placeholder="Email" variant="filled" type="email" mb={3} />
-                <Input placeholder="Password" variant="filled" type="password" mb={6} />
-                <Button colorScheme="teal" mb={3}>Log In</Button>
-                <Button onClick={toggleColorMode}>Toggle Color Mode</Button>
-            </Flex>
+            <Box>
+                <Text mb={6}>
+                    <Link href="/">Go back</Link>
+                </Text>
+                <Flex direction="column" background={formBackground} py={8} px={12} rounded={6}>
+                    <Heading mb={6}>Log in</Heading>
+                    <Input placeholder="Email" variant="filled" type="email" mb={3} />
+                    <Input placeholder="Password" variant="filled" type="password" mb={6} />
+                    <Button colorScheme="teal" mb={3}>Log In</Button>
+                    <Button onClick={toggleColorMode}>Toggle Color Mode</Button>
+                </Flex>
+            </Box>
         </Flex>
     )
 }
